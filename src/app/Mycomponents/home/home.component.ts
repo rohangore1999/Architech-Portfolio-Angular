9552060cdaed50134f0a5ea9a730d5d7803c8abb
Rohan Gore <gorerohan15@gmail.com>
2021-09-20 19:33:57 +0530

    first commit

---
+++ src/app/Mycomponents/home/home.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { animate, query, stagger, style, transition, trigger } from '@angular/animations';
+import {MatAccordion} from '@angular/material/expansion';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('fade', [
+      transition('void => *', [
+        style({ opacity: 0 }),
+        animate(2000, style({ opacity: 1 }))
+      ])
+    ]),
+
+    trigger('explainerAnim', [
+      transition('* => *',[
+        query('.tx',style({ opacity:0, transform: 'translateX(-40px'})),
+
+        query('.tx', stagger('500ms',[
+          animate('800ms 1.2s ease-out', style({opacity:1, transform: 'translateX(0)'}))
+        ]))
+      ])
+    ])
+  ]
+})
+export class HomeComponent implements OnInit {
+  @ViewChild(MatAccordion) accordion: MatAccordion;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { NavbarComponent } from './Mycomponents/navbar/navbar.component';
+import { HomeComponent } from './Mycomponents/home/home.component';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
